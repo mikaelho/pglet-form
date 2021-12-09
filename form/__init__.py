629a41d9b8f57dcefe43771df1db3f444553e5e5
mikaelho <mikael.honkala@gmail.com>
2021-12-09 17:41:27 +0000

    Version 0.1

--- form/__init__.py
+++ form/__init__.py
@@ -39,3 +39,41 @@ class Form(Stack):
     def __init__(self, width='90%', **kwargs):
         super().__init__(**kwargs)
         self.width = width
+        self.horizontal = False
+
+        if type(data) is type:
+            self.model = data
+            if hasattr(self.model, '__fields__'):
+                self.data = self.model.construct()
+            else:
+                self.data = self.model()
+        else:
+            self.model = type(data)
+            self.data = data
+        self.fields = {}
+
+        self.controls = _create_controls()
+
+    def _create_controls(self):
+        return [
+            self._create_control(attribute, attribute_type)
+            for attribute, attribute_type in self.model.__annotations__.items()
+        ]
+
+    def _create_control(self, attribute: str, attribute_type: Any):
+        label_text = attribute.replace('_', ' ').capitalize()
+        content_hint = ''
+
+        # pydantic support
+        pydantic_field = hasattr(self.model, '__fields__') and self.data.__fields__.get(attribute) or None
+
+        if pydantic_field:
+            label_text = pydantic_field.field_info.title or label_text
+            content_hint = pydantic_field.field_info.description or ''
+
+        control_type = from_data_type_to_form_control.get(attribute_type, TextBox)
+        control = control_type(label=label_text)
+        if control_type is TextBox:
+            control.placeholder = content_hint
+
+        self.fields[attribute] = control
